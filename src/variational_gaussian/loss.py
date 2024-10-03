a7575f2ac44a65b534d3b6e419885ce42b6c55cd
sem_k32 <the_manwho@mail.ru>
2024-10-03 14:29:08 +0300

    added prelimenary code design for variational gaussian

---
+++ src/variational_gaussian/loss.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+
+class addBayessianLoss():
+    def __init__(self, loss, bayessian_nn):
+        """change loss function for neural network for ELBO loss
+        """
+        self.init_loss = loss
+        self._bayessian_nn = bayessian_nn
+
+    def __call__(self, target, predict):
+        # compute kl-loss based on self._bayessian_nn
+        kl_loss = ...
+
+        return kl_loss + self.init_loss(target, predict)
---
+++ src/variational_gaussian/prior.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+from typing import Any
+
+
+class withGaussianPrior(type):
+    def __init__(cls, name, bases, namespace):
+        super(withGaussianPrior, cls).__init__(name, bases, namespace)
+
+    def _base_forward_pre_hook(module, *args):
+        # sample weights here
+        pass
+
+    def _linear_forward_pre_hook(module, *args, output):
+        # do local reparemtrization trick here
+        pass
+
+    def __call__(cls, *args: Any, **kwds: Any) -> Any:
+        """ rewrite submodules of the user's module. Samples weights from posterior to every submodule
+                before forward(). For Linear submodules uses local parametrization trick.
+        """
+        base_nn: nn.Module = super(withGaussianPrior, cls).__call__(*args, **kwds)
+
+        for module in base_nn.modules():
+            if type(module) is not nn.Linear:
+                module.register_forward_pre_hook(withGaussianPrior._base_forward_pre_hook)
+            else:
+                module.register_forward_pre_hook(withGaussianPrior._linear_forward_pre_hook)
+
+            # add means and stds for module's weights for sampling and ELBO-training
+            module.mean = nn.Parameter(...)
+            module.std = nn.Parameter(...)
+
+        # rewrite forward so it accepts new parameter - number of samples from posterior
+        base_nn.forward = ...
+
+        # this method prune weights of the model and return MAP-estimation of the NN.
+        # So user can use returned NN as usual nn.Module
+        base_nn.get_pruned = ...
+
+        return base_nn
+
+
