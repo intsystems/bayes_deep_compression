27041b42144d5ac9f2961fd680de467f627e3af0
sem-k32 <kirill.semkin32@mail.ru>
2024-10-31 18:09:53 +0300

    Conducted experiment on MNIST and got metrics. Futher improvemnets are coming

--- examples/variational_gaussian/MNIST/classification_nn.py
+++ examples/variational_gaussian/MNIST/classification_nn.py
@@ -8,14 +8,14 @@ class ClassificationNet(nn.Module):
     def __init__(self, img_size: tuple[int], num_classes: int, num_layers: int, hidden_size: int):
         super().__init__()
 
-        layers = []
+        layers = [nn.BatchNorm2d(1)]
         layers.append(nn.Flatten())
-        layers.extend([nn.Linear(img_size[0] * img_size[1], hidden_size), nn.Tanh()])
+        layers.extend([nn.Linear(img_size[0] * img_size[1] * img_size[2], hidden_size)])
 
         for i in range(num_layers - 2):
             if i % 2 == 0:
                 layers.append(nn.BatchNorm1d(hidden_size))
-            layers.extend([nn.Linear(hidden_size, hidden_size), nn.Tanh()])
+            layers.extend([nn.Linear(hidden_size, hidden_size), nn.ReLU()])
 
         layers.append(nn.Linear(hidden_size, num_classes))
         self.transform = nn.Sequential(*layers)
--- examples/variational_gaussian/MNIST/metrics.py
+++ examples/variational_gaussian/MNIST/metrics.py
@@ -4,14 +4,15 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from sklearn.metrics import accuracy_score, precision_score, recall_score
+from matplotlib import pyplot as plt
 
 from tqdm.notebook import tqdm
 
 
 def log_metrics(
-        step: int, 
-        net: nn.Module, 
-        test_loader: DataLoader, 
+        step: int,
+        net: nn.Module,
+        test_loader: DataLoader,
         device: torch.device,
         writer: SummaryWriter = None
 ) -> dict:
@@ -19,8 +20,8 @@ def log_metrics(
     logits = []
     targets = []
 
-    for imgs, target in tqdm(test_loader, desc="Test batchs", leave=True):
-        imgs = imgs.to(device)
+    for imgs, target in tqdm(test_loader, desc="Test batchs", leave=False):
+        imgs = imgs.to(device, torch.float32)
 
         logits.append(net(imgs))
         targets.append(target)
@@ -37,6 +38,27 @@ def log_metrics(
         for key, val in metrics.items():
             writer.add_scalar(key, val, step)
 
+        # show img classification
+        figs = []
+        NUM_EXAMPLES = 5
+        i = 0
+        test_loader_iter = iter(test_loader)
+        while i < NUM_EXAMPLES:
+            imgs, target = next(test_loader_iter)
+            prediction = net(imgs.to(device)).cpu().argmax(axis=1)
+
+            for j in range(test_loader.batch_size):
+                if i < NUM_EXAMPLES:
+                    fig, ax = plt.subplots()
+                    ax.imshow(imgs[j].squeeze().numpy(), cmap="gray")
+                    ax.set_title(f"Target = {target[j].item()}; prediction = {prediction[j].item()}")
+
+                    figs.append(fig)
+
+                    i += 1
+                else:
+                    break
+
+        writer.add_figure("Test/Examples", figs, step)
+
     return metrics
-
-
